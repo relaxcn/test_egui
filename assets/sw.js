192628ca9bffc5948e5a4ff6f6e2d2f3bb8e464b
RelaxCN <64476349+relaxcn@users.noreply.github.com>
2025-04-02 00:51:49 +0800

    a

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './snapnote.js',
+  './snapnote_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
